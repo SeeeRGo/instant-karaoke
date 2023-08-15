82d6a99bcd29b80303d524827496beefa317b5de
Sergey Mizin <seeergo@yahoo.com>
2023-08-15 19:00:30 +0400

    algo solved

--- subtitle.py
+++ subtitle.py
@@ -2,7 +2,7 @@ import whisper
 from manim import *
 
 def transcribe_file(filename):
-  model = whisper.load_model("base.en")
+  model = whisper.load_model("tiny.en")
   result = model.transcribe(filename, word_timestamps=True)
   return result['segments']
 
@@ -11,22 +11,36 @@ class IterateColor(Scene):
       segments = transcribe_file("Kitty In A Casket - Cold Black Heart.mp3")
       last_end = 0
       for segment in segments:
-        words = segment['words']
-        texts = []
-        for word in words:
-          # print('word: ', word['word'], 'start: ', word['start'], 'end: ', word['end'])
-          
-          texts.append((Text(word['word'], font_size=24), word['end'] - word['start'], word['start'] - last_end))
-          last_end = word['end']
+        # self.add(text1)
+        word_list_parsed = []
+        word_list = segment['words']
+        words = ''
+        total = 0
+        for w in word_list:
+            wait_time = w['start'] - last_end
+            last_end = w['end']
+            word_list_parsed.append((w['end'] - w['start'], len(w['word'].strip()), wait_time))
+            words += w['word'].strip()
+            total += len(w['word'].strip())
 
-        # print(texts)
-        for (word, duration, wait) in texts:
-          print('duration:', duration, 'wait: ', wait)
-          self.add(word)
-          # if wait > 0:
-          #   self.wait(wait)
-          self.play(word.animate.set_color(RED), run_time=duration)
-          self.remove(word)
+        text1 = Text(segment['text'], font_size=24)
+        self.add(text1)
+
+        offset = 0
+        for (duration, length, wait) in word_list_parsed:
+          if wait > 0:
+            self.wait(wait)
+          next_offset = offset + length
+          if offset + length > len(text1):
+            next_offset = len(text1)
+          for i in range(offset, next_offset):
+            print('offset: ', offset, 'offset + length: ', offset + length, 'i: ', i)
+            letter = text1[i]
+            self.play(letter.animate.set_color(RED), run_time=duration/length)
+
+          offset = next_offset
+
+        self.remove(text1)
 
 scene = IterateColor()
 scene.render()
--- transcribe.py
+++ transcribe.py
@@ -31,21 +31,37 @@ class IterateColor(Scene):
       filename = "Kitty In A Casket - Cold Black Heart.mp3"
       segments = transcribe_file(filename)
       separate_file(filename)
-      last_end = 0
+
       for segment in segments:
-        words = segment['words']
-        texts = []
+        word_list_parsed = []
+        word_list = segment['words']
+        words = ''
+        total = 0
+        for w in word_list:
+            wait_time = w['start'] - last_end
+            last_end = w['end']
+            word_list_parsed.append((w['end'] - w['start'], len(w['word'].strip()), wait_time))
+            words += w['word'].strip()
+            total += len(w['word'].strip())
 
-        for word in words:          
-          texts.append((Text(word['word'], font_size=24), word['end'] - word['start'], word['start'] - last_end))
-          last_end = word['end']
+        text1 = Text(segment['text'], font_size=24)
+        self.add(text1)
 
-        for (word, duration, wait) in texts:
+        offset = 0
+        for (duration, length, wait) in word_list_parsed:
           if wait > 0:
             self.wait(wait)
-          self.add(word)
-          self.play(word.animate.set_color(RED), run_time=duration)
-          self.remove(word)
+          next_offset = offset + length
+          if offset + length > len(text1):
+            next_offset = len(text1)
+          for i in range(offset, next_offset):
+            print('offset: ', offset, 'offset + length: ', offset + length, 'i: ', i)
+            letter = text1[i]
+            self.play(letter.animate.set_color(RED), run_time=duration/length)
+
+          offset = next_offset
+
+        self.remove(text1)
 
 scene = IterateColor()
 scene.render()
