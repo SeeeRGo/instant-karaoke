3d8a026909635f7a3511965b3f23b54da9a983f6
Мизин Сергей Юрьевич (4091265) <mizin@vtb.ru>
2023-08-16 15:25:23 +0400

    splitted transcription

--- transcribe_file.py
+++ transcribe_file.py
@@ -3,12 +3,12 @@ import whisper
 def transcribe_file(filename):
   model = whisper.load_model("large-v2")
   result = model.transcribe(filename, word_timestamps=True)
-  f = open("transcript.txt", "a")
+  f = open("transcript_splited.txt", "a")
   f.write(str(result))
   f.close()
   return result['segments']
 
-segments = transcribe_file("Kitty In A Casket - Cold Black Heart.mp3")
+segments = transcribe_file("./output/Kitty In A Casket - Cold Black Heart/vocals.wav")
 # # print(segments)
 
 # for segment in segments:
