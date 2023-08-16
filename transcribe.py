1167981d4fce1dbcecbd2ef3066aa4434b03ef8b
Sergey Mizin <seeergo@yahoo.com>
2023-08-16 13:21:12 +0400

    added transcript

--- transcribe.py
+++ transcribe.py
@@ -1,19 +1,19 @@
 import whisper
 from manim import *
-from spleeter.separator import Separator
-import ffmpeg
-from manim import config as global_config
+# from spleeter.separator import Separator
+# import ffmpeg
+# from manim import config as global_config
 
-config = global_config.copy()
-config.disable_caching = True
+# config = global_config.copy()
+# config.disable_caching = True
 
-def separate_file(filename):
-  separator = Separator('spleeter:2stems')
+# def separate_file(filename):
+#   separator = Separator('spleeter:2stems')
 
-  separator.separate_to_file(filename, './output')
+#   separator.separate_to_file(filename, './output')
 
 def transcribe_file(filename):
-  model = whisper.load_model("medium.en")
+  model = whisper.load_model("large-v2")
   result = model.transcribe(filename, word_timestamps=True)
   return result['segments']
 
@@ -29,10 +29,10 @@ def transcribe_file(filename):
 class IterateColor(Scene):
     def construct(self):
       filename = "Kitty In A Casket - Cold Black Heart.mp3"
-      segments = transcribe_file(filename)
-      separate_file(filename)
-      
+      segments = transcribe_file(filename)      
       last_end = 0
+      # separate_file(filename)
+      self.add_sound('./output/Kitty In A Casket - Cold Black Heart/vocals.wav')
       for segment in segments:
         word_list_parsed = []
         word_list = segment['words']
@@ -41,35 +41,36 @@ class IterateColor(Scene):
         for w in word_list:
             wait_time = w['start'] - last_end
             last_end = w['end']
-            word_list_parsed.append((w['end'] - w['start'], len(w['word'].strip()), wait_time))
-            words += w['word'].strip()
-            total += len(w['word'].strip())
-
-        text1 = Text(segment['text'], font_size=24)
+            word_list_parsed.append((w['end'] - w['start'], len(w['word'].replace(' ', '_')), wait_time))
+            words += w['word'].replace(' ', '_')
+            total += len(w['word'].replace(' ', '_'))
+        text = segment['text'].replace(' ', '_')
+        text1 = Text(text, font_size=24)
+        print(len(text1), total)
+        if total > len(text1):
+          text1 = Text(f'{text}.', font_size=24)
         self.add(text1)
-
         offset = 0
         for (duration, length, wait) in word_list_parsed:
           if wait > 0:
             self.wait(wait)
           next_offset = offset + length
-          if offset + length > len(text1):
-            next_offset = len(text1)
           for i in range(offset, next_offset):
-            print('offset: ', offset, 'offset + length: ', offset + length, 'i: ', i)
             letter = text1[i]
-            self.play(letter.animate.set_color(RED), run_time=duration/length)
+            run_time = round(duration * 90/length) / 90
+            self.play(letter.animate.set_color(RED), run_time=run_time)
 
           offset = next_offset
 
         self.remove(text1)
 
+
 scene = IterateColor()
 scene.render()
 
-input = ffmpeg.input('./media/videos/1080p60/IterateColor.mp4').video
-audio = ffmpeg.input('./output/Kitty In A Casket - Cold Black Heart/vocals.wav').audio
-out = ffmpeg.output(audio, input, 'out.mp4').run()
+# input = ffmpeg.input('./media/videos/1080p60/IterateColor.mp4').video
+# audio = ffmpeg.input('./output/Kitty In A Casket - Cold Black Heart/vocals.wav').audio
+# out = ffmpeg.output(audio, input, 'out.mp4').run()
 
 # from faster_whisper import WhisperModel
 
--- transcribe_file.py
+++ transcribe_file.py
@@ -1,15 +1,18 @@
 import whisper
 
 def transcribe_file(filename):
-  model = whisper.load_model("base.en")
+  model = whisper.load_model("large-v2")
   result = model.transcribe(filename, word_timestamps=True)
+  f = open("transcript.txt", "a")
+  f.write(str(result))
+  f.close()
   return result['segments']
 
 segments = transcribe_file("Kitty In A Casket - Cold Black Heart.mp3")
-# print(segments)
+# # print(segments)
 
-for segment in segments:
-  words = segment['words']
-  print('text: ', segment['text'])
-  for word in words:
-      print('word: ', word['word'], 'start: ', word['start'], 'end: ', word['end'])
+# for segment in segments:
+#   words = segment['words']
+#   print('text: ', segment['text'])
+#   for word in words:
+#       print('word: ', word['word'], 'start: ', word['start'], 'end: ', word['end'])
