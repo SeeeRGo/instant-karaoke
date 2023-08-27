ae791e95585a72050c0d92219c7fc54927908e91
Sergey Mizin <seeergo@yahoo.com>
2023-08-27 22:32:33 +0400

    toughest algorithm work in my career

--- match_text.py
+++ match_text.py
@@ -1,5 +1,7 @@
 import json
 from functools import reduce
+from jellyfish import soundex, match_rating_codex, nysiis, jaro_winkler_similarity, jaro_similarity, damerau_levenshtein_distance
+import replace_words
  
 def parse_segments(filename):
   f = open(filename)
@@ -7,7 +9,15 @@ def parse_segments(filename):
 
   words = []
   for segment in data['segments']:
-    words = words + segment['words']
+    words.append({
+      "text": segment['text'],
+      "words": [{
+        "code": nysiis(word["word"].strip()),
+        "word": word["word"].strip(),
+        "start": word["start"],
+        "end": word["end"]
+        } for word in segment['words']]
+    })
 
   return words
 
@@ -24,43 +34,56 @@ def parse_lyrics(filename):
   #close file
   text_file.close()
   words = list(reduce(lambda a,b: a + b, map(lambda line: line.split(' '), data.split('\n'))))
-  return words
+  return [{"code": nysiis(word), "word": word} for word in words]
   
 def match_text(transcript, actual_lyrics):
   words_timed = parse_segments(transcript)
-  len_timed = len(words_timed)
   words = parse_lyrics(actual_lyrics)
-  len_words = len(words)
 
-  result = []
-  words_i = 0
+  remaining_words = words
+  for segment in words_timed:
+    text = segment['text']
+    segment_codes = ' '.join([word["code"] for word in segment["words"]])
+    stop = False
+    best_match = 0
+    text_match = 0
+    actual_segment = ''
+    actual_segment_codes = ''
+    distance = 1000
+    while(stop != True):
+      for i,word in enumerate(remaining_words):
+        new_match_score = jaro_similarity(segment_codes, actual_segment_codes + word['code'])
+        text_match_score = jaro_similarity(text, actual_segment + word['word'])
+        new_distance = damerau_levenshtein_distance(segment_codes, actual_segment_codes + word['code'])
 
-  for i, word_timed in enumerate(words_timed):
-    timed = word_timed['word']
-    start = word_timed['start']
-    end = word_timed['end']
-    actual_text = words[word_i]
-    ratio = len(timed) / len(actual_text)
-    if ratio > 1.5:
-      while ratio > 1.5:
-        word_i += 1
-        actual_text += words[word_i]
-        ratio = len(timed) / len(actual_text)
-    if ratio < 0.66:
-      while ratio < 0.66:
-        timed += words_timed[i+1]['word']
-        ratio = len(timed) / len(actual_text)
-    
-    result.append({
-      start: word['start']
-      end: word['end']
-      word: actual_text
-    })
+        len_compare_continue = len(text) / len(actual_segment + word['word']) > 1.3
+        text_compare = text_match_score > text_match and new_distance < distance
 
-  print(result)
+        if new_match_score > best_match or text_compare or len_compare_continue:
+          best_match = new_match_score
+          text_match = text_match_score
+          distance = new_distance
+          actual_segment_codes += word['code']
+          actual_segment += word['word']
+          actual_segment_codes += ' '
+          actual_segment += ' '
+        else:
+          stop = True
+          remaining_words = remaining_words[i:]
+          break
+    # break
+    print(replace_words.replace_words(actual_segment.strip(), nums=segment['words']))
 
 
-    
 
-# parse_lyrics('lyrics.txt')
+def getSoundexList(dList):
+    res = [soundex(x) for x in dList]   # iterate over each elem in the dataList
+    # print(res)     # ['T000', 'F630', 'F630', 'D263', 'T000', 'D263']
+    return res
+
+# print([x for x in sorted(getSoundexList(parse_segments('transcript.json')))])
+
 match_text('transcript.json', 'lyrics.txt')
+# print(parse_lyrics('lyrics.txt'))
+# print(jaro_winkler_similarity('Staring on, perfection of love', 'Stared at in awe Perfection, no flaw'))
+# print(jaro_winkler_similarity('Staring on, perfection of love', 'Stared at in awe Perfection, no flaw'))
---
+++ replace_words.py
@@ -0,0 +1,113 @@
+from itertools import groupby
+
+nums = [
+  {"word": "one", "start": 1, "end": 2},
+  {"word": "two", "start": 2, "end": 3},
+  {"word": "three", "start": 3, "end": 4},
+  {"word": "four", "start": 4, "end": 5},
+  {"word": "five", "start": 5, "end": 6},
+  {"word": "six", "start": 6, "end": 7},
+  {"word": "seven", "start": 7, "end": 8},
+]
+nums_uneven_durations = [
+  {"word": "one", "start": 0.5, "end": 2},
+  {"word": "two", "start": 2, "end": 3},
+  {"word": "three", "start": 3, "end": 5},
+  {"word": "four", "start": 5, "end": 5.5},
+  {"word": "five", "start": 5.5, "end": 6},
+  {"word": "six", "start": 6, "end": 7},
+  {"word": "seven", "start": 7, "end": 10},
+]
+nums_uneven_durations_with_pauses = [
+  {"word": "one", "start": 0.5, "end": 2},
+  {"word": "two", "start": 2, "end": 3},
+  {"word": "three", "start": 4, "end": 5},
+  {"word": "four", "start": 5, "end": 5.5},
+  {"word": "five", "start": 5.5, "end": 6},
+  {"word": "six", "start": 6.5, "end": 7},
+  {"word": "seven", "start": 8, "end": 10},
+]
+
+def grouper(item):
+    """Будем использовать эту функцию для группировки сортировки."""
+    return item['index']
+
+def replace_words(new_text, nums=nums):
+  new_words = new_text.split(' ')
+  total_duration = 0
+  last_end = nums[0]["start"]
+  non_zero_wait = []
+  for i, num in enumerate(nums):
+    total_duration += num["end"] - num["start"]
+    wait = num['start'] - last_end
+    if wait > 0:
+      non_zero_wait.append({"index":i, "wait": wait })
+    last_end = num['end']
+  coef = len(nums) / len(new_words)
+  pos_coef = 1 / coef
+  non_zero_wait = groupby(sorted([{"index":round(x["index"] * pos_coef), "wait": x["wait"]} for x in non_zero_wait], key=grouper), key=grouper)
+  adjusted_wait = []
+  for key, group_items in non_zero_wait:
+    total_wait = 0
+    for item in group_items: 
+      total_wait += item['wait']
+    adjusted_wait.append({ "index": key, "wait": total_wait})
+
+  print(adjusted_wait)
+  res = []
+  current_start = nums[0]["start"]
+  if coef >= 1:
+    lesser_duration = 0
+    for i in range(0, len(new_words)):
+      lesser_duration += nums[i]["end"] - nums[i]["start"]
+    
+    coef = total_duration / lesser_duration
+    for i,word in enumerate(new_words):
+      wait = 0
+      for calc_wait in adjusted_wait:
+        if calc_wait["index"] == i:
+          wait = calc_wait['wait']
+
+      current_start += wait
+      res.append({
+        "word": word,
+        "start": current_start,
+        "end": current_start + (nums[i]["end"] - nums[i]["start"]) * coef,
+      })
+      current_start = current_start + (nums[i]["end"] - nums[i]["start"]) * coef
+  else:
+    shortened_nums = []
+    shortened_duration = 0
+    for num in nums:
+      shortened_nums.append({
+        "start": current_start,
+        "end": current_start + (num["end"] - num["start"]) * coef,
+      })
+      shortened_duration += (num["end"] - num["start"]) * coef
+      current_start = current_start + (num["end"] - num["start"]) * coef
+    extra_words = len(new_words) - len(nums)
+    extra_duration = total_duration - shortened_duration
+    extra_duration_per_word = extra_duration / extra_words
+    for i in range(0, extra_words):
+      shortened_nums.append({
+        "start": current_start,
+        "end": current_start + extra_duration_per_word,
+      })
+      current_start = current_start + extra_duration_per_word
+
+    current_start = shortened_nums[0]["start"]
+    for i,word in enumerate(new_words):
+      wait = 0
+      for calc_wait in adjusted_wait:
+        if calc_wait["index"] == i:
+          wait = calc_wait['wait']
+      current_start += wait
+      res.append({
+        "word": word,
+        "start": current_start,
+        "end": current_start + shortened_nums[i]["end"] - shortened_nums[i]["start"]
+      })
+      current_start = current_start + shortened_nums[i]["end"] - shortened_nums[i]["start"]
+  return res
+
+print(replace_words("new better shorter text average", nums=nums_uneven_durations_with_pauses))
